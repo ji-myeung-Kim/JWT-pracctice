cca59644ae3696e3a84f16611750d4393b18bc6e
kjm <wlaud5314@naver.com>
2021-11-19 00:27:34 +0900

    [Update] JwtAuthenticationFilter

--- jwt/src/main/java/com/cos/jwt/config/SecurityConfig.java
+++ jwt/src/main/java/com/cos/jwt/config/SecurityConfig.java
@@ -1,11 +1,17 @@
 package com.cos.jwt.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.context.SecurityContextPersistenceFilter;
 import org.springframework.web.filter.CorsFilter;
+
+import com.cos.jwt.config.jwt.JwtAuthenticationFilter;
+import com.cos.jwt.filter.MyFilter3;
 
 import lombok.RequiredArgsConstructor;
 
@@ -16,15 +22,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
 	private final CorsFilter corsFilter;
 	
+	@Bean
+	public BCryptPasswordEncoder PasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+		http.addFilterBefore(new MyFilter3(), SecurityContextPersistenceFilter.class);
 		http.csrf().disable();
 		//세션을 사용하지 않겠다 
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 		.and()
-		.addFilter(corsFilter) // @CrossOrigin(인증x),시큐리티 필터에 등록 인증 인증(O)
+		.addFilter(corsFilter) // @CrossOrigin(인증x),시큐리티 필터에 등록 인증(O)
 		.formLogin().disable()
 		.httpBasic().disable()
+		.addFilter(new JwtAuthenticationFilter(authenticationManager())) //Authentication
 		.authorizeRequests()
 		.antMatchers("/api/v1/user/**")
 		.access("hasRole('ROLE_USER') or hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')" )
@@ -32,7 +45,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		.access("hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')" )
 		.antMatchers("/api/v1/admin/**")
 		.access("hasRole('ROLE_ADMIN')" )
-		.anyRequest().permitAll();
+		.anyRequest().permitAll()   ;
 	
 	}
 }
